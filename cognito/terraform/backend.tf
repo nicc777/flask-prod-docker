e4208302a6eb4d1d79f3412d52ff8a4e1d215385
Nico Coetzee <nico@oculusd.com>
2020-03-20 06:14:18 +0200

    WiP

--- cognito/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-k0ioo07m"
-    encrypt = true
-    key    = "poc/flask-prod-docker/terraform.tfstate"
-    dynamodb_table = "terraform_state"
-    region = "us-east-1"
-  }
-}
